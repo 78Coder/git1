97bd95386db9ad67e1122759306a98ac1c72a197
78Coder <31921635+78Coder@users.noreply.github.com>
2020-04-15 22:02:11 +0800

    1

--- cn.hrbeu/src/Onc.java
+++ cn.hrbeu/src/Onc.java
@@ -1,2 +1,18 @@
 public class Onc {
+    public static void main(String[] args) {
+        boolean flag = true;
+        for (int i = 2; i <= 1000; i++) {
+            flag = true;
+            for (int j = 2; j < Math.sqrt(i)+1 ; j++) {
+                if (i % j == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == false){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
